c35ce42234a45ec5e4eec8cf05a5e0f4eae107e1
Kevin Deisz <kevin.deisz@gmail.com>
2017-08-05 14:16:41 -0400

    Simplify setup

--- extensions/extensions.rb
+++ extensions/extensions.rb
@@ -2,6 +2,15 @@ require 'digest/sha1'
 require 'parser'
 require 'tempfile'
 
+require 'extensions/ast_modifier'
+require 'extensions/ast_parser'
+require 'extensions/regex_modifier'
+require 'extensions/source_file'
+
+Dir[File.expand_path('extensions/modifiers/*', __dir__)].each do |modifier_source_file|
+  require modifier_source_file
+end
+
 module Extensions
   class << self
     attr_accessor :iseq_dir, :modifiers
@@ -27,24 +36,13 @@ module Extensions
     File.expand_path(File.join('..', '.iseq'), __dir__)
   FileUtils.mkdir_p(iseq_dir) unless File.directory?(iseq_dir)
 
-  self.modifiers = []
-end
-
-require 'extensions/ast_modifier'
-require 'extensions/ast_parser'
-require 'extensions/regex_modifier'
-require 'extensions/source_file'
-
-Dir[File.expand_path('extensions/modifiers/*', __dir__)].each do |modifier_source_file|
-  require modifier_source_file
-end
-
-Extensions.configure do |config|
-  config.add Extensions::Modifiers::DateSigil.new
-  config.add Extensions::Modifiers::URISigil.new
-  config.add Extensions::Modifiers::NumberSigil.new
-  config.add Extensions::Modifiers::TypeSafeMethodArgs.new
-  config.add Extensions::Modifiers::TypeSafeMethodReturns.new
+  self.modifiers = [
+    Modifiers::DateSigil.new,
+    Modifiers::URISigil.new,
+    Modifiers::NumberSigil.new,
+    Modifiers::TypeSafeMethodArgs.new,
+    Modifiers::TypeSafeMethodReturns.new
+  ]
 end
 
 RubyVM::InstructionSequence.singleton_class.prepend(Module.new do
