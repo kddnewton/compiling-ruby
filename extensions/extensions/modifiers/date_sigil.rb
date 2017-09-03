3a842e995e8a9b7c215c08dffa5002b9812ea8b9
Kevin Deisz <kevin.deisz@gmail.com>
2017-09-03 16:59:06 -0400

    Make date sigil more performant

--- extensions/extensions/modifiers/date_sigil.rb
+++ extensions/extensions/modifiers/date_sigil.rb
@@ -1,8 +1,13 @@
 module Extensions
   module Modifiers
     class DateSigil < RegexModifier
+      FORMAT = '%FT%T%:z'
+
       def initialize
-        super(/~d\((.+?)\)/, 'Date.parse("\1")')
+        super(/~d\((.+?)\)/) do |match|
+          date = Date.parse(match[3..-2])
+          "Date.strptime('#{date.strftime(FORMAT)}', '#{FORMAT}')"
+        end
       end
     end
   end
