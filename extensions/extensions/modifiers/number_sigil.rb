2fd00fc7bcb6038b6a0c2c334de86bf5c03d0286
Kevin Deisz <kevin.deisz@gmail.com>
2017-09-10 10:38:21 -0400

    Remove old version of vernacular

--- extensions/extensions/modifiers/number_sigil.rb
+++
@@ -1,9 +0,0 @@
-module Extensions
-  module Modifiers
-    class NumberSigil < RegexModifier
-      def initialize
-        super(/~n\(([\d\s+-\/*\(\)]+?)\)/) { |match| eval(match[3..-2]) }
-      end
-    end
-  end
-end
--- extensions/extensions/modifiers/uri_sigil.rb
+++
@@ -1,9 +0,0 @@
-module Extensions
-  module Modifiers
-    class URISigil < RegexModifier
-      def initialize
-        super(/~u\((.+?)\)/, 'URI.parse("\1")')
-      end
-    end
-  end
-end
